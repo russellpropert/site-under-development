2f168b2cef557e40a75fe7208c7a61afc82a2da6
Russell Propert <russellpropert@gmail.com>
2025-01-28 04:21:48 -0500

    Changing scroll event listener to wheel.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.addEventListener('scroll', (event) => {
+document.addEventListener('wheel', (event) => {
   event.preventDefault();
-}, { passive: false});
+}, { passive: false });
 
 document.addEventListener('touchstart', (event) => {
   event.preventDefault();
-}, { passive: false});
+}, { passive: false });
 
 document.addEventListener('touchmove', (event) => {
   event.preventDefault();
-}, { passive: false});
+}, { passive: false });
 
 document.addEventListener('touchend', (event) => {
   event.preventDefault();
-}, { passive: false});
+}, { passive: false });
